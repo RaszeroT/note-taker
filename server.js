746222e92e67029e5eedb73a8f87737e684b305c
Raszero <travisjcelli95@gmail.com>
2023-11-30 18:37:18 -0600

    GET previous notes from db.json and POST from webpage to db.json

--- server.js
+++ server.js
@@ -1,21 +1,17 @@
 const express = require("express");
-
+const note = require("./routes/note");
 const path = require("path");
 const PORT = 3001;
 
 const app = express();
 
-
 // middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-
-
-
+app.use(note);
 
 // html routes:
-
 // GET route for homepage
 app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/index.html"))
@@ -31,9 +27,7 @@ app.use("*", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/pages/404.html"));
 });
 
-
-
-
+// listen for port
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
 });
